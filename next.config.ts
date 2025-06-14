0b806a22142c9f015822519418f20dd605caaf31
sourav0809 <spathak7431@gmail.com>
2025-06-15 01:34:48 +0530

    feat: add configuration files for Next.js and PM2 deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
 export default nextConfig;
